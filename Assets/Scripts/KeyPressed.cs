0e39e5cc225a9d41e58506601fa7acd4ced03e88
pampas93 <abhijit.93@hotmail.com>
2017-11-29 02:30:44 -0500

    UI backgrounds, play time for 5 seconds

--- Assets/Scripts/KeyPressed.cs
+++ Assets/Scripts/KeyPressed.cs
@@ -13,19 +13,21 @@ public class KeyPressed : MonoBehaviour {
     private void Start()
     {
         mainSource = GameObject.Find("MainAudio").GetComponent<AudioSource>();
-
     }
 
+    //When a Key is clicked
     public void clicked()
     {
+        //Play the corresposing clip to the key
         mainSource.clip = KeyNote;
         mainSource.Play();
 
+        //Adding this keyNote into the record clip
         int i = KeyboardManager.instance.index;
         KeyboardManager.instance.myClip[i] = KeyNote;
-
         KeyboardManager.instance.index++;
 
+        //Adding the key into record area
         KeyboardManager.instance.AddKeyNote(Note);
     }
 
--- Assets/Scripts/KeyboardManager.cs
+++ Assets/Scripts/KeyboardManager.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class KeyboardManager : MonoBehaviour {
 
-    public static KeyboardManager instance;
+    public static KeyboardManager instance;         //Singleton Design
 
     [HideInInspector]
-    public AudioClip[] myClip;
+    public AudioClip[] myClip;                      //Final record clip
 
     [HideInInspector]
-    public int index = 0;
+    public int index = 0;                           //Keeping track of added clips in the recordClip
 
-    AudioSource mainSource;
+    AudioSource mainSource;                         //Main Audio source
 
-    public Transform recordCanvas;
+    public Transform recordCanvas;                  //Notes area or the record area
 
+
+    //Used for positioning the new Added keys in record area and for playstick
     private int x_position = -420;
     public int gap = 50;
 
     bool inside_canvas = true;
 
-    int totalKeys = 0;
+    int totalKeys = 0;                              //Total Keys added to the record clip
 
-    public GameObject playBack_stick;
-    private Vector3 playStick_initial_position;
+    public GameObject playBack_stick;               //Used to visualise during play
+    private Vector3 playStick_initial_position;     //Re initializing the position of playstick
 
-    private IEnumerator coroutine;
+    private IEnumerator coroutine;                  //Main coroutine to play the clip
+
+    System.DateTime timeStarted;                    //Used to keep track of play time
+
+    public int PlayDuration = 5;                    //Change in inspector (in seconds)
 
     private void Awake()
     {
@@ -37,14 +43,18 @@ public class KeyboardManager : MonoBehaviour {
 
     void Start()
     {
+        //Initializing the main Audio source, coroutine to play clip, and posiiton of playStick
+
         mainSource = GameObject.Find("MainAudio").GetComponent<AudioSource>();
         coroutine = PlayWholeRecord();
         playStick_initial_position = playBack_stick.transform.localPosition;
     }
 
+
+    //Adding a Keynote in the record Area
     public void AddKeyNote(GameObject Note)
     {
-        if (inside_canvas)
+        if (inside_canvas)      //Keynotes are added only if it can fit in the recordArea
         {
             GameObject newNote = Instantiate(Note, recordCanvas);
             Vector3 temp = newNote.transform.localPosition;
@@ -60,17 +70,28 @@ public class KeyboardManager : MonoBehaviour {
         }
     }
 
+
+    //When Play button is clicked
     public void playClip()
     {
-        Debug.Log(index);
+        timeStarted = System.DateTime.Now;
+       
+        //Debug.Log(index);
         playBack_stick.SetActive(true);
+
+        StopCoroutine(coroutine);
+
+        coroutine = null;
+        coroutine = PlayWholeRecord();
 
         StartCoroutine(coroutine);
     }
 
+
+    //Coroutine to play the recoreded clip
     IEnumerator PlayWholeRecord()
     {
-
+        playBack_stick.transform.localPosition = playStick_initial_position;
         for (int i = 0; i < totalKeys; i++)
         {
             AudioClip c = myClip[i];
@@ -78,25 +99,31 @@ public class KeyboardManager : MonoBehaviour {
             mainSource.Play();
             yield return new WaitForSeconds(c.length);
 
+            var playTime = System.DateTime.Now - timeStarted;            //If play time exceeds PlayDuration seconds stop playing
+            if (playTime.Seconds > PlayDuration)
+            {
+                break;
+            }
+
             Vector3 temp = playBack_stick.transform.localPosition;
             playBack_stick.transform.localPosition = new Vector3(temp.x + gap, temp.y, temp.z);
-            //mainSource.Play();
         }
 
         playBack_stick.SetActive(false);
         playBack_stick.transform.localPosition = playStick_initial_position;
         coroutine = null;
         coroutine = PlayWholeRecord();
-        //mainSource.Play();
-        //yield return new WaitForSeconds(seconds);
-        //Debug.Log("")
     }
 
+
+    //When Stop button is clicked
     public void stopClip()
     {
         StopCoroutine(coroutine);
     }
 
+
+    //When Erase button is clicked
     public void eraseRecord()
     {
         //clearing the coroutine and playstick
