2b6bda01e49ee025c3a2080339b2a11b43304f6e
pampas93 <abhijit.93@hotmail.com>
2017-11-29 00:41:15 -0500

    Keyboard setup with 8 keys

---
+++ Assets/Scripts/KeyPressed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPressed : MonoBehaviour {
+
+    public AudioClip KeyNote;
+
+    AudioSource mainSource;
+
+    private void Start()
+    {
+        mainSource = GameObject.Find("MainAudio").GetComponent<AudioSource>();
+
+    }
+
+    public void clicked()
+    {
+        mainSource.clip = KeyNote;
+        mainSource.Play();
+
+        int i = KeyboardManager.instance.index;
+        KeyboardManager.instance.myClip[i] = KeyNote;
+
+        KeyboardManager.instance.index++;
+    }
+}
---
+++ Assets/Scripts/KeyboardManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour {
+
+    public static KeyboardManager instance;
+
+    [HideInInspector]
+    public AudioClip[] myClip;
+
+    [HideInInspector]
+    public int index = 0;
+
+    AudioSource mainSource;
+
+    private void Awake()
+    {
+        instance = this;
+        myClip = new AudioClip[100];
+        index = 0;
+    }
+
+    void Start()
+    {
+        mainSource = GameObject.Find("MainAudio").GetComponent<AudioSource>();
+
+    }
+
+   
+}
